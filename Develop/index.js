15230faa96d354c034cf4a54dcc14bb791e7e9ef
aziz <mraziz.aden@gmail.com>
2023-07-08 16:03:09 -0400

    index

--- Develop/index.js
+++ Develop/index.js
@@ -65,10 +65,55 @@ inquirer
       name: 'email',
       validate: (input) => (input ? true : 'You need to input a value'),
     },
- 
+  ])
+  .then(({
+    name,
+    title,
+    description,
+    installation,
+    usage,
+    license,
+    contributors,
+    test,
+    github,
+    email,
+  }) => {
+    // Creating the layout of the Readme page
+    const ReadMeTemplate = `# ${title}
+![License](https://img.shields.io/static/v1?label=License&message=${license}&color=blueviolet&style=plastic)
+
+## Table of Contents:
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [Test](#test)
+* [Questions](#questions)
+
+### Description:
+${description}
+
+### Installation:
+To run this program, you need the following packages: ${installation}
+
+### Usage:
+Example of how to use this program: ${usage}
+
+### License:
+This project is licensed under: ${license}
+
+### Contributors:
+${contributors}
+
+### Test:
+To test, run the following command: ${test}
+
+### Questions:
+If you have any questions, contact ${name} on [GitHub](https://github.com/${github}) or [Email](mailto:${email})`;
+
     // Using the createNewFile function and passing the data through it
-    createNewFile('ReadMe.md', ReadMeTemplate)
-  );
+    createNewFile('ReadMe.md', ReadMeTemplate);
+  });
 
 // Function that takes a filename and data as parameters and uses them to write a file
 function createNewFile(fileName, data) {
